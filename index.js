1602daead9f17ed8493b850f3ced21a11d7dd397
ramimhossain12 <ramimhossain6242@gmail.com>
2021-06-25 22:31:08 +0600

    Find One Data

--- index.js
+++ index.js
@@ -1,90 +1,77 @@
- var MongoClient = require('mongodb').MongoClient;
+var MongoClient = require("mongodb").MongoClient;
 
- var URL =
-   "mongodb+srv://ramimTitle:xFfqsAJBt1w5kbh6@cluster0.0tm16.mongodb.net?retryWrites=true&w=majority";
- 
+var URL =
+  "mongodb+srv://ramimTitle:xFfqsAJBt1w5kbh6@cluster0.0tm16.mongodb.net?retryWrites=true&w=majority";
 
-   var config = { useUnifiedTopology: true };
+var config = { useUnifiedTopology: true };
 
-   MongoClient.connect(URL,config,function(error,MyMongoClinet){
+MongoClient.connect(URL, config, function (error, MyMongoClinet) {
+  if (error) {
+    console.log("con fail");
+  } else {
+    console.log("con success");
+    //InsertData(MyMongoClinet);
+    //DeleteOneItem(MyMongoClinet);
+    //DeleteAllItem(MyMongoClinet);
+    findWithOutCondition(MyMongoClinet);
+  }
+});
 
-    if(error){
-            console.log("con fail");
-    }
-    else{
-          console.log("con success");
-          //InsertData(MyMongoClinet); 
-          //DeleteOneItem(MyMongoClinet);
-          DeleteAllItem(MyMongoClinet);
-    }
-   })
-
-
-
-
-
-
-   //Insert Data
-
+//Insert Data
 
 function InsertData(MyMongoClinet) {
-
   var MyDataBase = MyMongoClinet.db("school");
-   var MyCollection =   MyDataBase.collection("students");
+  var MyCollection = MyDataBase.collection("students");
 
-   var MyData = {Name :"Lima",Roll:"2",Class:"Ten",City:"Raj"};
+  var MyData = { Name: "Lima", Roll: "2", Class: "Ten", City: "Raj" };
 
-   MyCollection.insertOne(MyData,function(error){
-
-    if(error){
-          console.log("con insert fail");
+  MyCollection.insertOne(MyData, function (error) {
+    if (error) {
+      console.log("con insert fail");
+    } else {
+      console.log("con insert success");
     }
-    else{
-         console.log("con insert success");
-    }
-   })
+  });
 }
-
-
-
 
 // //Delete One Data
 
 function DeleteOneItem(MyMongoClinet) {
   var MyDataBase = MyMongoClinet.db("school");
-   var MyCollection = MyDataBase.collection("students");
-   
-   var DeleteItem = {Roll: "2"}
-   MyCollection.deleteOne(DeleteItem,function(error){
+  var MyCollection = MyDataBase.collection("students");
 
+  var DeleteItem = { Roll: "2" };
+  MyCollection.deleteOne(DeleteItem, function (error) {
     if (error) {
       console.log("Data Delete fail");
     } else {
       console.log("Data Delete  success");
     }
-
-
-   });
+  });
 }
-
-
 
 // //Delete All Data
 
 function DeleteAllItem(MyMongoClinet) {
   var MyDataBase = MyMongoClinet.db("school");
-   var MyCollection = MyDataBase.collection("students");
-   
+  var MyCollection = MyDataBase.collection("students");
 
-   MyCollection.deleteMany(function(error,Resultobj){
-
+  MyCollection.deleteMany(function (error, Resultobj) {
     if (error) {
       console.log("Data Delete fail");
     } else {
-      console.log(Resultobj.result.n +"Item Delteed");
+      console.log(Resultobj.result.n + "Item Delteed");
     }
-
-
-   });
+  });
 }
 
+//find obj
+
+function findWithOutCondition(MyMongoClinet) {
+  var MyDataBase = MyMongoClinet.db("school");
+  var MyCollection = MyDataBase.collection("students");
+  var FindObj = {};
+  MyCollection.findOne(FindObj, function (error, result) {
+    console.log(result);
+  });
+}
